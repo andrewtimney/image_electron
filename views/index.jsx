f94032d943ba444f778e5616e64f5ea4aa1faa13
Andrew Timney <andrew.timney@gmail.com>
2015-10-14 20:50:13 +0100

    Remove knockout, render react view

--- views/index.jsx
+++ views/index.jsx
@@ -1,36 +1,58 @@
 var ipc = require('ipc');
-var configSettings = require('../configsettings/config.js'); 
 var fileStuff = require('../services/file-stuff');
 var exifData = require('../services/exif-data');
-var Q = require('Q');
 var moment = require('moment');
 var _ = require('lodash');
-var http = require('http');
+var React = require('react');
 
-var viewModel = {
-  count: ko.observable(0),
-  files: ko.observableArray(),
-  newly: ko.observableArray(),
-  config: ko.observable(),
-  cities: ko.observableArray(),
-  imageSize: ko.observable('100px')
-};
-ko.applyBindings(viewModel);
-
-ipc.on('exif-complete', function(arg) {
-   complete(arg);
-});
- 
-ipc.on('on-files', function(arg) {
-  if(arg.old.length){
-    viewModel.files(arg.old);
-    console.log(arg.old.slice(-1), arg.old.slice(0, 1));
+var Images = React.createClass({
+  getInitialState(){
+    return { files:[] };
+  },
+  componentWillMount(){
+    ipc.on('exif-complete', this.getNewPics);
+    ipc.on('on-files', this.getOldPics);
+    ipc.send('get-files');
+  },
+  getNewPics(){
+  },
+  getOldPics(files){
+    this.setState({ files: files.old });
+    ipc.send('get-exif', files);
+  },
+  render(){
+    var files = [];
+    
+    this.state.files.forEach((file) => {
+      var divStyle = { backgroundImage: file.stylePath };
+      files.push(<span className="image flex-item square" style={divStyle}></span>);
+    });
+  
+    return <div className="flex-container">
+            {files}
+           </div>;
   }
-  ipc.send('get-exif', arg);
 });
 
-ipc.send('get-files');
+//ipc.on('exif-complete', function(arg) {
+  // complete(arg);
+//});
+ 
+//ipc.on('on-files', function(arg) {
+//  if(arg.old.length){
+//    viewModel.files(arg.old);
+//    console.log(arg.old.slice(-1), arg.old.slice(0, 1));
+//  }
+//  ipc.send('get-exif', arg);
+//});
 
-function complete(pics){
-   viewModel.newly(pics.newly);
-}
+//ipc.send('get-files');
+
+//function complete(pics){
+//   viewModel.newly(pics.newly);
+//}
+
+React.render(
+	<Images />,
+	document.getElementById('images')
+);
