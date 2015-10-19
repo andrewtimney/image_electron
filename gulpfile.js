6782171064d7f421277aa49bbf0357b9789b3c87
Andrew Timney <andrew.timney@gmail.com>
2015-10-19 22:15:12 +0100

    Added gulp run electron task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var babel = require("gulp-babel");
+var runElectron = require("gulp-run-electron");
 
-gulp.task("default", function () {
+gulp.task("babel", function () {
   return gulp.src("views/**/**.jsx")
     .pipe(babel())
     .pipe(gulp.dest("views/build"));
+});
+
+gulp.task('default', ['babel'], function(){
+  gulp.src(".")
+    .pipe(runElectron([], {}));
 });
